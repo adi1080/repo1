0d7ada14ff46caeee3d0c78f6e7e8857916d28e1
adi1080 <adityaborsarkar@gmail.com>
2024-11-26 10:17:08 +0530

    Update SpringBootProfilesApplication.java
    h

--- SpringBoot-profiles/src/main/java/com/cjc/main/SpringBootProfilesApplication.java
+++ SpringBoot-profiles/src/main/java/com/cjc/main/SpringBootProfilesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootProfilesApplication {
 
 	public static void main(String[] args) {
+		system.out.println("message from github");
 		System.out.println("springboot profiles with  github");
 		SpringApplication.run(SpringBootProfilesApplication.class, args);
 	}
